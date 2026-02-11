caf2f191227612b2cb926c7d57c5f8607da6b36b
Obed-Is <obedaquino71@gmail.com>
2026-02-10 22:45:11 -0600

    fix(webhook): prevenir parseo JSON para validación de firma Stripe

--- index.js
+++ index.js
@@ -10,9 +10,14 @@ const portApp = process.env.SERVER_PORT || 4000;
 const __dirname = import.meta.dirname;
 const stripeController = new StripeController();
 
-app.use(express.json());
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')));
+//webhook de stripe
+//ponerlo antes del middleware de express.json() evita q express convierta el body en objeto
+app.post('/webhook', express.raw({ type: 'application/json' }), (req, res) => stripeController.stripeWebhook(req, res))
+
+app.use(express.json());
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, './views/main.html')));
 app.get('/prices', (req, res) => stripeController.getPricesStripe(req, res));
@@ -20,9 +25,7 @@ app.get('/success', (req, res) => stripeController.redirectEndHook(req, res));
 
 app.post('/create-products', (req, res) => stripeController.stripeCreateProducts(req, res));
 app.post('/create-checkout-session', (req, res) => stripeController.saleInStripe(req, res));
-//webhook de stripe
-//con este middleware evitamos parsear el body para q la firma de stripe sea valida
-app.post('/webhook', express.raw({ type: 'application/json' }), (req, res) => stripeController.stripeWebhook(req, res))
+
 app.post('/valid-payment', (req, res) => stripeController.validatePayment(req, res));
 
 app.listen(portApp, () => {
